71c2806576f2509a9707b64a1292a20a0b20e744
Misha Grinko <glukym@gmail.com>
2024-04-29 14:27:19 +0300

    prepare for CI/CD

--- src/persistence/index.js
+++ src/persistence/index.js
@@ -1,2 +1,2 @@
-if (process.env.POSTGRES_HOST) module.exports = require('./postgres');
-else module.exports = require('./sqlite');
+if (process.env.POSTGRES_HOST) module.exports = require('./postgres')
+else module.exports = require('./sqlite')
